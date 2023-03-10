a41109c3dc19c2aac12bd77de9877e366aec560f
paderbapar2022 <64470606+paderbapar2022@users.noreply.github.com>
2023-03-10 23:47:33 +0600

    Update fbkingrahim.py

--- fbkingrahim.py
+++ fbkingrahim.py
@@ -1,23 +1,4 @@
-#SC MAKED BY Rahim AFRIDI 🤣 and Fuked By FB-KING 🙄
-#Tera pappa FB-KING 😘 MAHIN AHMED
-#................♥Rahim♥................
-import os,sys,time,json,random,re,string,platform,base64,uuid
-from bs4 import BeautifulSoup as sop
-from bs4 import BeautifulSoup
-import requests as ress
-from datetime import date
-from datetime import datetime
-from time import sleep
-from os import system as s
-from time import sleep as waktu
-try:
-    import requests
-    from concurrent.futures import ThreadPoolExecutor as ThreadPool
-    import mechanize
-    from requests.exceptions import ConnectionError
-except ModuleNotFoundError:
-    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
-    os.system('pip install bs4')
+
 RED = '\033[1;91m'
 WHITE = '\033[1;97m'
 GREEN = '\033[1;32m' #
